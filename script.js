bacea94197b089f210e1f382e197cc5d7d51c579
Muhammadqasim1011 <akhundqasim1011@gmail.com>
2024-09-09 11:45:07 +0500

    update

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 let time = document.getElementById('time');
+let date = document.getElementById('date');
+let days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
 setInterval(() => {
     let toDay = new Date();
-    let milli = toDay.getMilliseconds();
+    // let milli = toDay.getMilliseconds();
     let sec = toDay.getSeconds();
     let min = toDay.getMinutes();
     let hour = toDay.getHours();
+    let dayname = toDay.getDay();
+    let dateString = toDay.toLocaleDateString();    
     let ampm = hour >= 12 ? 'PM' : 'AM';
 
     hour = hour % 12;
     if (hour === 0) {
         hour = 12;
     }    
-    if (milli < 10) {
-        milli = '00' + milli;
-    } else if (milli < 100) {
-        milli = '0' + milli;
-    }
+    // if (milli < 10) {
+    //     milli = '00' + milli;
+    // } else if (milli < 100) {
+    //     milli = '0' + milli;
+    // }
     if (sec < 10) {
         sec = '0' + sec;
     }
@@ -29,5 +33,6 @@ setInterval(() => {
         hour = '0' + hour;
     }
 
-    time.innerHTML = `${hour}:${min}:${sec}:${milli} ${ampm}`;
-}, 1);
+    time.innerHTML = `${days[dayname]} ${hour}:${min}:${sec} ${ampm}`;
+    date.innerHTML = dateString;
+}, 1000);
